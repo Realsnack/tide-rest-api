60145a210c40589c1c522074f854f14fd23d0ed6
Michal Svačina <msvacina@outlook.com>
2024-02-17 00:16:41 +0100

    Added root handlers and updated routees

--- src/api/handlers/root_handlers.rs
+++ src/api/handlers/root_handlers.rs
@@ -1,3 +1,8 @@
-fn handle_get() {
+pub async fn handle_get(mut req: tide::Request<()>) -> tide::Result {
+    Ok(tide::Response::builder(tide::StatusCode::Ok).body("Hello, World!").build())
+}
 
+pub async fn handle_get_with_name(mut req: tide::Request<()>) -> tide::Result {
+    let name = req.param("name").unwrap_or("Error");
+    Ok(tide::Response::builder(tide::StatusCode::Ok).body(format!("Hello, {}!", name)).build())
 }
--- src/routes/root_routes.rs
+++ src/routes/root_routes.rs
@@ -1,3 +1,4 @@
 pub fn init_root_routes(app: &mut tide::Server<()>) {
-    app.at("/").get(|_| async { Ok(tide::Response::builder(tide::StatusCode::Ok).body("Hello, World!").build()) });
+    app.at("/").get(crate::api::handlers::root_handlers::handle_get);
+    app.at("/:name").get(crate::api::handlers::root_handlers::handle_get_with_name);
 } 
