c71ba7fe5f151a23103bca1102f83d8e4a2ef94e
Michal Svačina <msvacina@outlook.com>
2024-02-18 23:02:28 +0100

    Updated key not found condition

--- src/api/handlers/redis_handlers.rs
+++ src/api/handlers/redis_handlers.rs
@@ -8,6 +8,10 @@ pub async fn handle_get_key_value(req: tide::Request<AppState>) -> tide::Result 
     redis_key.set_expiration(key_expiration);
     tide::log::debug!("Key: {} has value: {:?}", key, redis_key);
 
+    if redis_key.value.is_none() {
+        return Ok(tide::Response::builder(tide::StatusCode::NotFound).body(tide::Body::from_json(&redis_key).unwrap()).build());
+    }
+
     Ok(tide::Response::builder(tide::StatusCode::Ok).body(tide::Body::from_json(&redis_key).unwrap()).build())
 }
 
