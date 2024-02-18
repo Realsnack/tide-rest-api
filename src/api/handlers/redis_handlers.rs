11af91c53bbee43a71c74eefe98d91fd12af18af
Michal Svačina <msvacina@outlook.com>
2024-02-18 22:59:28 +0100

    Updated return type for redis handler

--- src/api/handlers/redis_handlers.rs
+++ src/api/handlers/redis_handlers.rs
@@ -1,6 +1,6 @@
 use crate::AppState;
 
-pub async fn handle_get_key_value(req: tide::Request<AppState>) -> tide::Result<tide::Body> {
+pub async fn handle_get_key_value(req: tide::Request<AppState>) -> tide::Result {
     let key = req.param("key").unwrap_or("Error");
     let redis = &req.state().redis;
     let mut redis_key = redis.get_key(key);
@@ -8,7 +8,6 @@ pub async fn handle_get_key_value(req: tide::Request<AppState>) -> tide::Result<
     redis_key.set_expiration(key_expiration);
     tide::log::debug!("Key: {} has value: {:?}", key, redis_key);
 
-    // Ok(tide::Response::builder(tide::StatusCode::Ok).body(tide::Body::from_json(&redis_value)).build())
-    Ok(tide::Body::from_json(&redis_key).unwrap())
+    Ok(tide::Response::builder(tide::StatusCode::Ok).body(tide::Body::from_json(&redis_key).unwrap()).build())
 }
 
