79913d07a7c3947d712eae3e946c79b1e69dddc9
Michal Svačina <msvacina@outlook.com>
2024-02-22 19:27:51 +0100

    Added base route for redis API

--- src/routes/redis_routes.rs
+++ src/routes/redis_routes.rs
@@ -1,6 +1,15 @@
 use crate::AppState;
 
 pub fn init_redis_routes(app: &mut tide::Server<AppState>) {
-    app.at("/api/redis/:key").get(crate::api::handlers::redis_handlers::handle_get_redis_key);
-    app.at("/api/redis").post(crate::api::handlers::redis_handlers::handle_post_redis_key);
+    let state = app.state().clone();
+
+    app.at("/api/redis").nest({
+        let mut api = tide::Server::with_state(state);
+
+        api.at("/:key").get(crate::api::handlers::redis_handlers::handle_get_redis_key);
+        api.at("/").post(crate::api::handlers::redis_handlers::handle_post_redis_key);
+
+        api
+    });
 }
+
